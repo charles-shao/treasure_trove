9af6e17dc232e9a36c1a252fee979f11e3f3628a
Charles Shao <Charles@Charless-MacBook-Air.local>
2013-05-31 20:09:02 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreasureTrove::Application.config.secret_token = 'eb312e11a434ee4b49b732c358a1ac631c56cfd7d253bb12f0af1c4a7f8da647360c11f49d72619c879a511d140991238f8ef4be12cd03ff324c6ed217668ceb'
