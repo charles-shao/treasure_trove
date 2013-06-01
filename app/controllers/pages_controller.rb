a624164f553096954d3ceac69e3c0a203ef94fba
Ilya Anisimoff <ilya@intersect.org.au>
2013-06-01 11:47:15 +1000

    Moved connectors to lib/

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
+require 'trove_connector'
+
 class PagesController < ApplicationController 
 
+  TROVE = TroveConnector.new 'u6uep5gs1dh99d6u'
+
+
   def index
+    puts TROVE.query_articles 'Winston'
   end
 
 end
