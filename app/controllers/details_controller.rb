1dd1e5e6a892b9bfa203766c443ae5e53625629a
Ilya Anisimoff <ilya@intersect.org.au>
2013-06-01 12:51:06 +1000

    Makes article text html safe

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,12 +1,16 @@
 require 'trove_connector'
+require 'open_calais_connector'
 require 'pp'
 
 class DetailsController < ApplicationController
 
   TROVE = TroveConnector.new 'u6uep5gs1dh99d6u'
+  OC = OpenCalaisConnector.new 'r8hxmgzrxwz85yp7trnz2yyq'
 
   def show
     @article = TROVE.get_article params['article']
-    pp @article
+    @enriched = OC.enrich @article['articleText']
+    pp @results
+    pp @enriched
   end
 end
