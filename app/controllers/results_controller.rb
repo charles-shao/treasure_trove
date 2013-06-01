0a30d451f68933994e654d8115d33751acb9917a
khanh2907 <khanh@intersect.org.au>
2013-06-01 13:55:05 +1000

    Add validation for search, results are now htmlsafe, pdf iframe for article and d3.js files

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -6,8 +6,12 @@ class ResultsController < ApplicationController
   TROVE = TroveConnector.new 'u6uep5gs1dh99d6u'
 
   def index
-  	@search_params = params[:search_param]
-  	@results = TROVE.query_articles @search_params
+  	if !params[:search_param].empty?
+  		@search_params = params[:search_param]
+  		@results = TROVE.query_articles @search_params
+  	else
+  		redirect_to root_path, :flash => { :error => "Search cannot be empty" }
+  	end
 
   end
 end
