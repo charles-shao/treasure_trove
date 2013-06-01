c77fa63c10289871778b315f2394675e3417932b
vintage <vintage@vintage-530U3C-530U4C.(none)>
2013-06-01 15:25:21 +1000

    Add location detail function handler

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,13 +1,6 @@
-/**
- * Created with JetBrains RubyMine.
- * User: vintage
- * Date: 1/06/13
- * Time: 1:41 PM
- * To change this template use File | Settings | File Templates.
- */
 var geocoder;
 var map;
-
+var infowindow = new google.maps.InfoWindow();
 function initialize() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(-34.397, 150.644);
@@ -20,20 +13,42 @@ function initialize() {
 }
 
 function codeAddress() {
-    var address = document.getElementById('address').value; // REPLACE WITH STRING TO LOOK UP DIV TAG
+    var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
 
             map.setCenter(results[0].geometry.location);
-
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
-
+            map.setZoom(17);
+            var request = {
+                location: results[0].geometry.location,
+                radius: 100
+            };
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(request, callback);
         } else {
             alert('Geocode was not successful for the following reason: ' + status);
         }
+    });
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
     });
 }
 
