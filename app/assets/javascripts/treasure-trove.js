3cab4455c9daa7c26eba918147c106f4f1a0bc1a
charles <charles@intersect.org.au>
2013-06-01 11:13:41 +1000

    update details styling

--- app/assets/javascripts/treasure-trove.js
+++ app/assets/javascripts/treasure-trove.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-	$("#details-tab > li").on('click', function(){ 
-		$("li.active").removeClass("active");
-		$(this).addClass("active");
-	});
+//	$("#details-tab > li").on('click', function(){
+//		$("li.active").removeClass("active");
+//		$(this).addClass("active");
+//	});
+//
 
 });
