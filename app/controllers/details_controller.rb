685ed7858ff66f90917f3bfdd3a72f35c9d5ea15
Stanley Hon <stanleyhon348@gmail.com>
2013-06-01 17:33:32 +1000

    Fixed some debug prints in powerhouse_connector.rb, added array of entities to map to details_controller.rb

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -4,6 +4,9 @@ require 'pp'
 
 class DetailsController < ApplicationController
 
+  # This is an array of all the entities maps should try and map!
+  mapTheseEntities = ['City', 'Company', 'Continent', 'Country', 'Facility', 'Natural Feature', 'Organization', 'Place', 'ProvinceOrState', 'Region', 'Company Location', 'Address', 'Geo', 'ProvinceOrState']
+
   TROVE = TroveConnector.new 'u6uep5gs1dh99d6u'
   OC = OpenCalaisConnector.new 'r8hxmgzrxwz85yp7trnz2yyq'
 
