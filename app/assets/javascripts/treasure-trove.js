7eaff7f85f29bc757e30be6e030e49f19dc8eb3f
khanh2907 <khanh@intersect.org.au>
2013-06-01 19:43:51 +1000

    DBPedia for entities

--- app/assets/javascripts/treasure-trove.js
+++ app/assets/javascripts/treasure-trove.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
 
+	$(".entity-link").on('click', function(){
+		if ($(this).attr("loaded") == "false"){
+			var target = $(this).attr("data-target");
+			var value = $(this).attr('entity_name');
+			
+			$.ajax({
+				url: '/details/get_dbp',
+				type: 'get',
+				async: false,
+				dataType: 'text',
+				data: {value: value},
+				success: function(data) {
+					$(target + " > .well").append(data);
+					console.log(data);
+				},
+				error: function(request, error){
+					console.log("Something went wrong");
+					alert(request + ":" + error);
+				}
+			});
+
+			$(this).attr("loaded", "true");
+		}
+		
+
+		//
+	});
+
 });
